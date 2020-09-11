39344b0f25f316bd3913200d68743263bdb74356
Zach Laine <whatwasthataddress@gmail.com>
2020-09-11 18:10:52 -0500

    Initial checkin of skeleton, based on previous Boost projects.

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,62 @@
+# Copyright Louis Dionne 2016
+# Copyright Zach Laine 2016
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+###############################################################################
+# Boost
+###############################################################################
+set(Boost_USE_STATIC_LIBS ON)
+find_package(Boost 1.71.0 COMPONENTS ${boost_components})
+if (Boost_INCLUDE_DIR)
+  add_library(boost INTERFACE)
+  target_include_directories(boost INTERFACE ${Boost_INCLUDE_DIR})
+else ()
+  if (NOT BOOST_BRANCH)
+    set(BOOST_BRANCH master)
+  endif()
+  if (NOT EXISTS ${CMAKE_BINARY_DIR}/boost_root)
+    message("-- Boost was not found; it will be cloned locally from ${BOOST_BRANCH}.")
+    add_custom_target(
+      boost_root_clone
+      git clone --depth 100 -b ${BOOST_BRANCH}
+        https://github.com/boostorg/boost.git boost_root
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    if (MSVC)
+      set(bootstrap_cmd ./bootstrap.bat)
+    else()
+      set(bootstrap_cmd ./bootstrap.sh)
+    endif()
+    add_custom_target(
+      boost_clone
+      COMMAND git submodule init libs/assert
+      COMMAND git submodule init libs/config
+      COMMAND git submodule init libs/core
+      COMMAND git submodule init tools/build
+      COMMAND git submodule init libs/headers
+      COMMAND git submodule init tools/boost_install
+      COMMAND git submodule update --jobs 3
+      COMMAND ${bootstrap_cmd}
+      COMMAND ./b2 headers
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost_root
+      DEPENDS boost_root_clone)
+  endif()
+  add_library(boost INTERFACE)
+  add_dependencies(boost boost_clone)
+  target_include_directories(boost INTERFACE ${CMAKE_BINARY_DIR}/boost_root)
+endif ()
+
+
+###############################################################################
+# GoogleTest
+###############################################################################
+add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.10.0)
+target_include_directories(gtest      INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
+target_include_directories(gtest_main INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
+
+
+###############################################################################
+# Google Benchmark
+###############################################################################
+add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark-v1.1.0)
+target_include_directories(benchmark INTERFACE ${CMAKE_HOME_DIRECTORY}/benchmark-v1.1.0/include)
---
+++ include/boost/program_options_2/config.hpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2020 T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_PROGRAM_OPTIONS_2_CONFIG_HPP
+#define BOOST_PROGRAM_OPTIONS_2_CONFIG_HPP
+
+// Included for definition of __cpp_lib_concepts.
+#include <iterator>
+
+
+#ifdef BOOST_PROGRAM_OPTIONS_2_DOXYGEN
+
+/** Boost.ProgramOptions2 will automatically use concepts to constrain templates when
+    building in C++20 mode, if the compiler defines `__cpp_lib_concepts`.  To
+    disable the use of concepts, define this macro. */
+#    define BOOST_PROGRAM_OPTIONS_2_DISABLE_CONCEPTS
+
+#endif
+
+#if defined(__cpp_lib_concepts) && !defined(BOOST_PROGRAM_OPTIONS_2_DISABLE_CONCEPTS)
+#define BOOST_PROGRAM_OPTIONS_2_USE_CONCEPTS 1
+#else
+#define BOOST_PROGRAM_OPTIONS_2_USE_CONCEPTS 0
+#endif
+
+#endif
